3cd92392ddc08bef374940a02f2c648de13ebc59
Rhys Collier <rhys.collier@hotmail.co.uk>
2019-09-19 17:03:16 +0100

    feat: returns list of times for inputs of size 5,000 to 100,000

--- timing-framework.rb
+++ timing-framework.rb
@@ -1,7 +1,16 @@
 def code_timer
-  arr = ("a" * 1_000_000).split('')
-  start_time = Time.now
-  arr.last
-  finish_time = Time.now
-  puts finish_time - start_time
+  input_size = 5_000
+  timing_array = Hash.new
+  while input_size <= 100_000
+    testing_array = *(1..input_size)
+    start_time = Time.now
+    testing_array.reverse
+    finish_time = Time.now
+    time_taken = finish_time - start_time
+    timing_array[input_size] = time_taken
+    input_size += 5_000
+  end
+  timing_array.each do |size, time|
+    puts "#{size}: #{time}"
+  end
 end
