c0887783a08f42d936679a5955814dabb0bab375
Rhys Collier <rhys.collier@hotmail.co.uk>
2019-09-23 17:19:30 +0100

    feat: create AlgorithmTimer class

--- timing-framework.rb
+++ timing-framework.rb
@@ -1,58 +1,60 @@
-INPUT_SIZE_LOWER_LIMIT = 5_000
-INPUT_SIZE_UPPER_LIMIT = 100_000
-INPUT_SIZE_INCREMENT = 5_000
+class AlgorithmTimer
+  INPUT_SIZE_LOWER_LIMIT = 5_000
+  INPUT_SIZE_UPPER_LIMIT = 100_000
+  INPUT_SIZE_INCREMENT = 5_000
 
-def algorithm_timer(algorithm_to_test)
-  timings_hash = Hash.new
-  51.times do
-    input_size = INPUT_SIZE_LOWER_LIMIT
-    while input_size <= INPUT_SIZE_UPPER_LIMIT
-      testing_array = (1..input_size).to_a
-      testing_array.shuffle! if algorithm_to_test == 'sort'
-      time_taken = time_taken_to_run_algorithm(algorithm_to_test, testing_array)
-      add_to_timings_hash(timings_hash, input_size, time_taken)
-      input_size += INPUT_SIZE_INCREMENT
+  def time_algorithm(algorithm_to_test)
+    timings_hash = Hash.new
+    65.times do
+      input_size = INPUT_SIZE_LOWER_LIMIT
+      while input_size <= INPUT_SIZE_UPPER_LIMIT
+        testing_array = (1..input_size).to_a
+        testing_array.shuffle! if algorithm_to_test == 'sort'
+        time_taken = time_taken_to_run_algorithm(algorithm_to_test, testing_array)
+        add_to_timings_hash(timings_hash, input_size, time_taken)
+        input_size += INPUT_SIZE_INCREMENT
+      end
+    end
+    print_timings(timings_hash)
+  end
+
+  private
+
+  def add_to_timings_hash(timings_hash, input_size, time_taken)
+    if timings_hash.has_key?(input_size)
+      timings_hash[input_size].push time_taken
+    else
+      timings_hash[input_size] = [time_taken]
     end
   end
-  print_timings(timings_hash)
-end
 
-private
-
-def add_to_timings_hash(timings_hash, input_size, time_taken)
-  if timings_hash.has_key?(input_size)
-    timings_hash[input_size].push time_taken
-  else
-    timings_hash[input_size] = [time_taken]
+  def median_value(timings_array)
+    return timings_array.sort[32]
   end
-end
 
-def median_value(timings_array)
-  return timings_array.sort[25]
-end
-
-def time_taken_to_run_algorithm(algorithm_to_test, testing_array)
-  start_time = Time.now
-  algorithm_under_test(algorithm_to_test, testing_array)
-  finish_time = Time.now
-  return finish_time - start_time
-end
-
-def print_timings(timings_hash)
-  timings_hash.each do |size, timings_array|
-    puts "#{size}: #{median_value(timings_array)}"
+  def time_taken_to_run_algorithm(algorithm_to_test, testing_array)
+    start_time = Time.now
+    algorithm_under_test(algorithm_to_test, testing_array)
+    finish_time = Time.now
+    return finish_time - start_time
   end
-end
 
-def algorithm_under_test(algorithm_to_test, testing_array)
-  case algorithm_to_test
-  when 'reverse'
-    testing_array.reverse
-  when 'last'
-    testing_array.last
-  when 'shuffle'
-    testing_array.shuffle
-  when 'sort'
-    testing_array.sort
+  def print_timings(timings_hash)
+    timings_hash.each do |size, timings_array|
+      puts "#{size}: #{median_value(timings_array)}"
+    end
+  end
+
+  def algorithm_under_test(algorithm_to_test, testing_array)
+    case algorithm_to_test
+    when 'reverse'
+      testing_array.reverse
+    when 'last'
+      testing_array.last
+    when 'shuffle'
+      testing_array.shuffle
+    when 'sort'
+      testing_array.sort
+    end
   end
 end
