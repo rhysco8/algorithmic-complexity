68d9d58dad38a1b3ed600e9a2c3552c53df5f6f9
Rhys Collier <rhys.collier@hotmail.co.uk>
2019-09-23 15:08:57 +0100

    feat: aggregate times for running algorithm 10 times

--- timing-framework.rb
+++ timing-framework.rb
@@ -1,16 +1,31 @@
 def code_timer
-  input_size = 5_000
-  timing_array = Hash.new
-  while input_size <= 100_000
-    testing_array = *(1..input_size)
-    start_time = Time.now
-    testing_array.reverse
-    finish_time = Time.now
-    time_taken = finish_time - start_time
-    timing_array[input_size] = time_taken
-    input_size += 5_000
+  timings_hash = Hash.new
+  10.times do
+    input_size = 5_000
+    while input_size <= 100_000
+      testing_array = *(1..input_size)
+      start_time = Time.now
+      testing_array.reverse
+      finish_time = Time.now
+      time_taken = finish_time - start_time
+      add_to_timings_hash(timings_hash, input_size, time_taken)
+      input_size += 5_000
+    end
   end
-  timing_array.each do |size, time|
-    puts "#{size}: #{time}"
+  timings_hash.each do |size, timings_array|
+    puts "#{size}: #{average_of_timings(timings_array)}"
   end
+end
+
+def add_to_timings_hash(timings_hash, input_size, time_taken)
+  if timings_hash.has_key?(input_size)
+    timings_hash[input_size].push time_taken
+  else
+    timings_hash[input_size] = [time_taken]
+  end
+  return timings_hash
+end
+
+def average_of_timings(timings_array)
+  return timings_array.sum.fdiv(timings_array.size)
 end
