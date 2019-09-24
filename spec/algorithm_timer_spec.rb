4ba703f8d27b9a5a624d450606a5499f4d6b40ab
Rhys Collier <rhys.collier@hotmail.co.uk>
2019-09-24 17:11:28 +0100

    test: time_algorithm outputs to std_out

--- spec/algorithm_timer_spec.rb
+++ spec/algorithm_timer_spec.rb
@@ -18,4 +18,13 @@ describe AlgorithmTimer do
       expect(@timer.custom_reverse([1, 2, 3])).to eq([3, 2, 1])
     end
   end
+
+  describe '#time_algorithm' do
+    describe "displays timings for chosen method" do
+      it ".reverse" do
+        table_of_timings = /\A5000: \d+\.?\d(e-\d+|\d*)\n10000: \d+\.?\d(e-\d+|\d*)\n15000: \d+\.?\d(e-\d+|\d*)\n20000: \d+\.?\d(e-\d+|\d*)\n25000: \d+\.?\d(e-\d+|\d*)\n30000: \d+\.?\d(e-\d+|\d*)\n35000: \d+\.?\d(e-\d+|\d*)\n40000: \d+\.?\d(e-\d+|\d*)\n45000: \d+\.?\d(e-\d+|\d*)\n50000: \d+\.?\d(e-\d+|\d*)\n55000: \d+\.?\d(e-\d+|\d*)\n60000: \d+\.?\d(e-\d+|\d*)\n65000: \d+\.?\d(e-\d+|\d*)\n70000: \d+\.?\d(e-\d+|\d*)\n75000: \d+\.?\d(e-\d+|\d*)\n80000: \d+\.?\d(e-\d+|\d*)\n85000: \d+\.?\d(e-\d+|\d*)\n90000: \d+\.?\d(e-\d+|\d*)\n95000: \d+\.?\d(e-\d+|\d*)\n100000: \d+\.?\d(e-\d+|\d*)\n/
+        expect{ @timer.time_algorithm('reverse') }.to output(table_of_timings).to_stdout
+      end
+    end
+  end
 end
