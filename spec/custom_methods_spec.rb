0835e5567466edf7152cbd1a83bace1f4f534d95
Rhys Collier <rhys.collier@hotmail.co.uk>
2019-09-25 14:05:11 +0100

    fix: include instead of extend CustomMethods

--- spec/custom_methods_spec.rb
+++ spec/custom_methods_spec.rb
@@ -1,29 +1,30 @@
 require 'custom_methods'
 
 describe CustomMethods do
-  let(:dummy_class) { Class.new { extend CustomMethods } }
+  let(:dummy_class) { Class.new { include CustomMethods } }
+  let(:dummy_class_instance) { dummy_class.new }
 
   describe '#custom_reverse' do
     it "returns [1] when passed [1]" do
-      expect(dummy_class.custom_reverse([1])).to eq([1])
+      expect(dummy_class_instance.custom_reverse([1])).to eq([1])
     end
 
     it "returns [2, 1] when passed [1, 2]" do
-      expect(dummy_class.custom_reverse([1, 2])).to eq([2, 1])
+      expect(dummy_class_instance.custom_reverse([1, 2])).to eq([2, 1])
     end
 
     it "returns [3, 2, 1] when passed [1, 2, 3]" do
-      expect(dummy_class.custom_reverse([1, 2, 3])).to eq([3, 2, 1])
+      expect(dummy_class_instance.custom_reverse([1, 2, 3])).to eq([3, 2, 1])
     end
   end
 
   describe '#custom_shuffle' do
     it "returns [1] when passed [1]" do
-      expect(dummy_class.custom_shuffle([1])).to eq([1])
+      expect(dummy_class_instance.custom_shuffle([1])).to eq([1])
     end
 
     it "returns shuffled array when passed [1, 2, 3, 4]" do
-      expect(dummy_class.custom_shuffle([1, 2, 3, 4])).to_not eq([1, 2, 3, 4])
+      expect(dummy_class_instance.custom_shuffle([1, 2, 3, 4])).to_not eq([1, 2, 3, 4])
     end
   end
 end
