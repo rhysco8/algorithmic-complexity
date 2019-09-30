a3fd191b0d87c2555f477d54ba5e66670865d4f4
Rhys Collier <rhys.collier@hotmail.co.uk>
2019-09-30 16:27:51 +0100

    feat: optimise custom_shuffle method to uses pop instead of delete_at

--- lib/custom_methods.rb
+++ lib/custom_methods.rb
@@ -13,7 +13,8 @@ module CustomMethods
       random_element_index = rand(0..array.size - 1)
       randomly_chosen_element = array[random_element_index]
       shuffled_array.push randomly_chosen_element
-      array.delete_at(random_element_index)
+      array[random_element_index] = array.last
+      array.pop
     end
     return shuffled_array
   end
